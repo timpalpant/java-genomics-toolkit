379a5e71889b985850ab42c0169c5d417d06c50a
Timothy Palpant <tim@palpant.us>
2011-11-19 08:44:45 -0500

    Initial upload of project with JCommander, JTransforms, and log4j libraries

---
+++ src/edu/unc/genomics/Autocorrelation.java
@@ -0,0 +1,13 @@
+package edu.unc.genomics;
+
+public class Autocorrelation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
