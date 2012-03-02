31b76bf64718864392a1c85e2f7cca55d0f2c1a1
Timothy Palpant <tim@palpant.us>
2012-03-01 19:44:13 -0500

    Fix galaxy config file and DNAProperty option for DNAPropertyCalculator

--- src/edu/unc/genomics/DNAPropertyConverter.java
+++ src/edu/unc/genomics/DNAPropertyConverter.java
@@ -3,25 +3,16 @@ package edu.unc.genomics;
 import org.genomeview.dnaproperties.DNAProperty;
 
 import com.beust.jcommander.IStringConverter;
-import com.beust.jcommander.ParameterException;
 
 /**
  * @author timpalpant
  *
  */
 public class DNAPropertyConverter implements IStringConverter<DNAProperty> {
-
-	private static final DNAProperty[] properties = DNAProperty.values();
 	
 	@Override
 	public DNAProperty convert(String value) {
-		for (DNAProperty p : properties) {
-			if (p.toString().equalsIgnoreCase(value)) {
-				return p;
-			}
-		}
-		
-		throw new ParameterException("Unknown or unavailable DNA property: " + value);
+		return DNAProperty.create(value);
 	}
 
 }
