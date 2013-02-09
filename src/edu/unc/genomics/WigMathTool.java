3c9788596c5ee6e22565a57e672b87810d44dfc9
Timothy Palpant <tim@palpant.us>
2013-02-09 09:25:46 -0500

    Add multithreading to wig math tools. Set default nTasks=1 since it is usually not helpful (IO >> compute). Update PredictFAIRESignal

--- src/edu/unc/genomics/WigMathTool.java
+++ src/edu/unc/genomics/WigMathTool.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
@@ -23,57 +26,66 @@ import edu.unc.genomics.io.WigFileWriter;
  * Abstract base class for writing programs to do computation on Wig files
  * Concrete subclasses must implement the compute method
  * 
- * WigMathTool takes all input Wig files, finds the intersecting set
- * of chromosomes with data, and then iterates through the inputs in a chunk-by-chunk
- * fashion, calling compute() on each chunk
+ * WigMathTool takes all input Wig files, finds the intersecting set of
+ * chromosomes with data, and then iterates through the inputs in a
+ * chunk-by-chunk fashion, calling compute() on each chunk
  * 
- * The compute method must return the output values for that chunk (one value for each base pair)
- * which will then be written into a new output Wig file.
+ * The compute method must return the output values for that chunk (one value
+ * for each base pair) which will then be written into a new output Wig file.
  * 
  * @author timpalpant
- *
+ * 
  */
 public abstract class WigMathTool extends CommandLineTool {
-	
+
 	private static final Logger log = Logger.getLogger(WigMathTool.class);
-	
-	@Parameter(names = {"-c", "--chunk"}, description = "Size to chunk each chromosome into when processing (bp)")
+
+	@Parameter(names = { "-c", "--chunk" }, description = "Size to chunk each chromosome into when processing (bp)")
 	public int chunkSize = DEFAULT_CHUNK_SIZE;
-	@Parameter(names = {"-f", "--fixedstep"}, description = "Force fixedStep output")
+	@Parameter(names = { "-p", "--threads" }, description = "Number of threads to use")
+	public int nThreads = 1;
+	@Parameter(names = { "-f", "--fixedstep" }, description = "Force fixedStep output")
 	public boolean fixedStep = false;
-	@Parameter(names = {"-o", "--output"}, description = "Output file", required = true)
+	@Parameter(names = { "-o", "--output" }, description = "Output file", required = true)
 	public Path outputFile;
-	
+
+	private ExecutorService pool;
+
 	/**
-	 * Holds all of the input Wig files for this compute job. 
-	 * Used to find the intersecting set of chromosomes to process
+	 * Holds all of the input Wig files for this compute job. Used to find the
+	 * intersecting set of chromosomes to process
 	 */
 	protected List<WigFileReader> inputs = new ArrayList<WigFileReader>();
-	
+
 	protected void addInputFile(WigFileReader wig) {
 		inputs.add(wig);
 	}
-	
+
 	/**
-	 * Setup the computation. Should add all input Wig files with addInputFile() during setup
+	 * Setup the computation. Should add all input Wig files with addInputFile()
+	 * during setup
 	 */
 	protected abstract void setup();
-	
+
 	/**
-	 * Do the computation on a chunk and return the results
-	 * Must return chunk.length() values (one for every base pair in chunk)
+	 * Do the computation on a chunk and return the results Must return
+	 * chunk.length() values (one for every base pair in chunk)
 	 * 
-	 * @param chunk the interval to process
+	 * @param chunk
+	 *            the interval to process
 	 * @return the results of the computation for this chunk
 	 * @throws IOException
 	 * @throws WigFileException
 	 */
 	protected abstract float[] compute(Interval chunk) throws IOException, WigFileException;
-	
+
 	@Override
 	public final void run() throws IOException {
 		log.debug("Executing setup operations");
 		setup();
+		
+		log.debug("Initializing thread pool");
+		pool = Executors.newFixedThreadPool(nThreads);
 		
 		log.debug("Processing files and writing result to disk");
 		try (WigFileWriter writer = new WigFileWriter(outputFile, TrackHeader.newWiggle())) {
@@ -89,52 +101,71 @@ public abstract class WigMathTool extends CommandLineTool {
 				while (bp < stop) {
 					int chunkStart = bp;
 					int chunkStop = Math.min(bp+chunkSize-1, stop);
-					Interval chunk = new Interval(chr, chunkStart, chunkStop);
+					final Interval chunk = new Interval(chr, chunkStart, chunkStop);
+					log.debug("Queueing chunk "+chunk);
 					
-					log.debug("Processing chunk "+chunk);
-					float[] result;
-					try {
-						result = compute(chunk);
-					} catch (WigFileException | IOException e) {
-						throw new CommandLineToolException("Exception while processing chunk "+chunk, e);
-					}
-					
-					// Verify that the computation returned the correct number of values for the chunk
-					if (result.length != chunk.length()) {
-						log.error("Expected result length="+chunk.length()+", got="+result.length);
-						throw new CommandLineToolException("Result of Wig computation is not the expected length!");
-					}
+					pool.submit(new Runnable() {
 
-					// Write the result of the computation for this chunk to disk
-					if (fixedStep) {
-						writer.writeFixedStepContig(new Contig(chunk, result));
-					} else {
-						writer.write(new Contig(chunk, result));
-					}
+                        @Override
+                        public void run() {
+                        	log.debug("Processing chunk "+chunk);
+                        	float[] result;
+        					try {
+        						result = compute(chunk);
+        					} catch (WigFileException | IOException e) {
+        						throw new CommandLineToolException("Exception while processing chunk "+chunk, e);
+        					}
+        					
+        					// Verify that the computation returned the correct number of values for the chunk
+        					if (result.length != chunk.length()) {
+        						log.error("Expected result length="+chunk.length()+", got="+result.length);
+        						throw new CommandLineToolException("Result of Wig computation is not the expected length!");
+        					}
+
+        					// Write the result of the computation for this chunk to disk
+        					if (fixedStep) {
+        						writer.writeFixedStepContig(new Contig(chunk, result));
+        					} else {
+        						writer.write(new Contig(chunk, result));
+        					}
+                        }
+					  
+					});
 					
 					// Move to the next chunk
 					bp = chunkStop + 1;
 				}
 			}
+			
+			pool.shutdown();
+			try {
+				pool.awaitTermination(10, TimeUnit.DAYS);
+			} catch (InterruptedException e) {
+				throw new CommandLineToolException(e);
+			}
 		} finally {
 			close();
 		}
 	}
-	
+
 	/**
 	 * Close the input files
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	private void close() throws IOException {
 		for (WigFileReader wig : inputs) {
 			wig.close();
 		}
 	}
-	
+
 	/**
 	 * Gets the highest start base for a chromosome amongst all wigs
-	 * @param wigs a List of wig files
-	 * @param chr the chromosome to get the most conservative start base for
+	 * 
+	 * @param wigs
+	 *            a List of wig files
+	 * @param chr
+	 *            the chromosome to get the most conservative start base for
 	 * @return the highest start base amongst all of the Wig files in wigs
 	 */
 	public static int getMaxChrStart(List<WigFileReader> wigs, String chr) {
@@ -144,44 +175,49 @@ public abstract class WigMathTool extends CommandLineTool {
 				max = wig.getChrStart(chr);
 			}
 		}
-		
+
 		return max;
 	}
-	
+
 	/**
 	 * Gets the lowest stop base for a chromosome amongst all wigs
-	 * @param wigs a List of wig files
-	 * @param chr the chromosome to get the most conservative stop base for
+	 * 
+	 * @param wigs
+	 *            a List of wig files
+	 * @param chr
+	 *            the chromosome to get the most conservative stop base for
 	 * @return the lowest stop base amongst all of the Wig files in wigs
 	 */
 	public static int getMinChrStop(List<WigFileReader> wigs, String chr) {
 		if (wigs.size() == 0) {
 			return -1;
 		}
-		
+
 		int min = Integer.MAX_VALUE;
 		for (WigFileReader wig : wigs) {
 			if (wig.getChrStop(chr) < min) {
 				min = wig.getChrStop(chr);
 			}
 		}
-		
+
 		return min;
 	}
-	
+
 	/**
 	 * Get the set of chromosomes that are held in common by all input files
-	 * @param wigs a list of Wig files to get the common chromosomes of
+	 * 
+	 * @param wigs
+	 *            a list of Wig files to get the common chromosomes of
 	 * @return the set of chromosomes held in common by all Wig files in wigs
 	 */
 	public static Set<String> getCommonChromosomes(List<WigFileReader> wigs) {
 		if (wigs == null || wigs.size() == 0) {
 			return new HashSet<String>();
 		}
-		
+
 		Set<String> chromosomes = wigs.get(0).chromosomes();
 		Iterator<String> it = chromosomes.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			String chr = it.next();
 			for (WigFileReader wig : wigs) {
 				if (!wig.includes(chr)) {
--- src/edu/unc/genomics/nucleosomes/PredictFAIRESignal.java
+++ src/edu/unc/genomics/nucleosomes/PredictFAIRESignal.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.log4j.Logger;
 
 import com.beust.jcommander.Parameter;
@@ -18,31 +19,37 @@ import edu.unc.genomics.ReadablePathValidator;
 import edu.unc.genomics.WigMathTool;
 import edu.unc.genomics.io.WigFileReader;
 import edu.unc.genomics.io.WigFileException;
-import edu.unc.utils.InclusionExclusion;
 
 /**
- * Attempt to predict FAIRE signal from nucleosome occupancy data using a simple probabilistic model
+ * Attempt to predict FAIRE signal from nucleosome occupancy data using a simple
+ * probabilistic model
+ * 
  * @author timpalpant
- *
+ * 
  */
 public class PredictFAIRESignal extends WigMathTool {
 
 	private static final Logger log = Logger.getLogger(PredictFAIRESignal.class);
 
-	@Parameter(names = {"-i", "--input"}, description = "Nucleosome occupancy (Wig)", required = true, validateWith = ReadablePathValidator.class)
+	@Parameter(names = { "-i", "--input" }, description = "Nucleosome dyad density (Wig)", required = true, validateWith = ReadablePathValidator.class)
 	public Path inputFile;
-	@Parameter(names = {"-s", "--sonication"}, description = "Sonication distribution", required = true, validateWith = ReadablePathValidator.class)
+	@Parameter(names = { "-s", "--sonication" }, description = "Sonication distribution", required = true, validateWith = ReadablePathValidator.class)
 	public Path sonicationFile;
-	@Parameter(names = {"-e", "--efficiency"}, description = "FAIRE efficiency / crosslinking coefficient")
+	@Parameter(names = { "-e", "--efficiency" }, description = "FAIRE crosslinking efficiency [0,1]")
 	public float crosslink = 1;
-	@Parameter(names = {"-x", "--extend"}, description = "In silico read extension (bp), -1 for paired-end")
+	@Parameter(names = { "-x", "--extend" }, description = "Single-end read extension (bp); -1 for paired-end")
 	public int extend = 250;
+	@Parameter(names = { "-n", "--nuc-size" }, description = "Nucleosome size (bp)")
+	public int nucSize = 147;
 
 	WigFileReader reader;
 	float[] sonication = new float[100];
 	int minL = Integer.MAX_VALUE, maxL = 0;
+	DescriptiveStatistics occupancyStats = new DescriptiveStatistics();
 	float maxOcc;
-	
+	float[] pNuc, pOcc;
+	Cache pOccCache, pAndCache;
+
 	@Override
 	public void setup() {
 		try {
@@ -51,22 +58,23 @@ public class PredictFAIRESignal extends WigMathTool {
 			throw new CommandLineToolException(e);
 		}
 		addInputFile(reader);
-		
+
 		log.debug("Loading sonication fragment length distribution");
 		float total = 0;
-		try(BufferedReader reader = Files.newBufferedReader(sonicationFile, Charset.defaultCharset())) {
+		try (BufferedReader reader = Files.newBufferedReader(sonicationFile, Charset.defaultCharset())) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				// Parse the line
 				String[] entry = line.split("\t");
 				if (entry.length != 2) {
-					throw new CommandLineToolException("Invalid format for sonication distribution file");
+					throw new CommandLineToolException(
+							"Invalid format for sonication distribution file (length\tpercent)");
 				}
 				int length = Integer.parseInt(entry[0]);
 				float percent = Float.parseFloat(entry[1]);
 				// Expand the sonication distribution array if necessary
 				if (length >= sonication.length) {
-					sonication = Arrays.copyOf(sonication, Math.max(sonication.length+100, length+1));
+					sonication = Arrays.copyOf(sonication, Math.max(sonication.length + 100, length + 1));
 				}
 				if (length < minL) {
 					minL = length;
@@ -82,136 +90,305 @@ public class PredictFAIRESignal extends WigMathTool {
 			e.printStackTrace();
 			throw new CommandLineToolException("Error loading sonication fragment length distribution");
 		}
+		if (maxL < extend) {
+			maxL = extend;
+		}
+		// We need an additional nucSize/2 overhang
+		maxL += nucSize / 2;
 		// Truncate the array to the minimum possible size
-		sonication = Arrays.copyOfRange(sonication, 0, maxL+1);
-		log.debug("Loaded sonication distribution for lengths: "+minL+"-"+maxL+"bp");
-		
+		sonication = Arrays.copyOfRange(sonication, 0, maxL + 1);
+		log.debug("Loaded sonication distribution for lengths: " + minL + "-" + maxL + "bp");
+
 		// Normalize the sonication distribution so that it has total 1
 		for (int i = 0; i < sonication.length; i++) {
 			sonication[i] /= total;
 		}
-		
-		// Store the maximum occupancy
-		maxOcc = (float) reader.max();
+
+		// Calculate the maximum dyad density in any nucleosome-sized window
+		// (i.e. maximum occupancy), for normalization
+		// so that the dyad counts represent probabilities
+		log.debug("Initializing statistics");
+		occupancyStats.setWindowSize(nucSize);
+
+		log.debug("Computing maximum genome-wide occupancy (normalization factor)");
+		for (String chr : reader.chromosomes()) {
+			occupancyStats.clear();
+
+			// Walk the chromosome while keeping track of occupancy
+			int bp = reader.getChrStart(chr);
+			int stop = reader.getChrStop(chr);
+			while (bp <= stop) {
+				int chunkStart = bp;
+				int chunkStop = Math.min(chunkStart + DEFAULT_CHUNK_SIZE - 1, stop);
+
+				try {
+					float[] data = reader.query(chr, chunkStart, chunkStop).getValues();
+					for (int i = 0; i < data.length; i++) {
+						if (Float.isNaN(data[i])) {
+							data[i] = 0;
+						}
+
+						occupancyStats.addValue(data[i]);
+						if (occupancyStats.getSum() > maxOcc) {
+							maxOcc = (float) occupancyStats.getSum();
+						}
+					}
+				} catch (WigFileException | IOException e) {
+					log.error("Error getting data from input Wig file");
+					e.printStackTrace();
+					throw new CommandLineToolException("Error getting data from input Wig file");
+				}
+
+				bp = chunkStop + 1;
+			}
+		}
+		log.debug("Computed maximum genome-wide occupancy = " + maxOcc);
 	}
-	
+
 	@Override
 	public float[] compute(Interval chunk) throws IOException, WigFileException {
-		int paddedStart = Math.max(chunk.getStart()-maxL, reader.getChrStart(chunk.getChr()));
-		int paddedStop = Math.min(chunk.getStop()+maxL, reader.getChrStop(chunk.getChr()));
-		
+		int paddedStart = Math.max(chunk.getStart() - maxL, reader.getChrStart(chunk.getChr()));
+		int paddedStop = Math.min(chunk.getStop() + maxL, reader.getChrStop(chunk.getChr()));
+
 		Contig data = reader.query(chunk.getChr(), paddedStart, paddedStop);
-		float[] result = data.get(chunk.getStart()-maxL, chunk.getStop()+maxL);
-		
-		// Scale the occupancy by the maximum occupancy so that it represents
-		// the probability that a base pair is occupied by a nucleosome
-		for (int i = 0; i < result.length; i++) {
-			result[i] /= maxOcc;
+		pNuc = data.get(chunk.getStart() - maxL, chunk.getStop() + maxL);
+
+		// Scale the dyad density by the maximum occupancy so that it represents
+		// the probability that a nucleosome is positioned at that base pair
+		// You should probably remove outliers (esp. CNVs) first
+		// Also save the occupancy at each base pair (for efficiency)
+		occupancyStats.clear();
+		pOcc = new float[pNuc.length];
+		for (int i = 0; i < pNuc.length; i++) {
+			pNuc[i] /= maxOcc;
+			occupancyStats.addValue(pNuc[i]);
+			if (i - nucSize / 2 >= 0) {
+				pOcc[i - nucSize / 2] = (float) occupancyStats.getSum();
+			}
 		}
-		
+
+		// Reset the memoization caches since we updated pNuc and pOcc
+		// Since pOcc() is a function on the lattice (x,l), where
+		// x \in [nucSize/2, pNuc.length-nucSize/2]
+		// l \in [minL, maxL]
+		// we preallocate with these dimensions: pNuc.length x (maxL+1)
+		// This cache will be large if the chunk size is too big
+		pOccCache = new Cache(pNuc.length, maxL + 1);
+		pAndCache = new Cache(pNuc.length, maxL + 1);
+
 		float[] prediction;
 		if (extend < 0) {
-			prediction = paired(chunk, result);
+			prediction = paired(chunk);
 		} else {
-			prediction = single(chunk, result);
+			prediction = single(chunk);
 		}
-		
+
 		return prediction;
 	}
-	
+
 	/**
 	 * Single-end prediction, with artificial uniform extension
+	 * 
 	 * @param chunk
-	 * @param occ
-	 * @return
+	 * @param pNuc
+	 * @return single-end FAIRE signal for the chunk
 	 */
-	private float[] single(Interval chunk, float[] occ) {
-		float[] watson = new float[occ.length];
-		float[] crick = new float[occ.length];
+	private float[] single(Interval chunk) {
+		float[] watson = new float[pNuc.length];
+		float[] crick = new float[pNuc.length];
 		// Consider all possible fragment lengths
-		for (int i = minL; i <= maxL; i++) {
+		for (int l = minL; l < sonication.length; l++) {
 			// No need to count if there are no fragments of this length
-			if (sonication[i] == 0) {
+			if (sonication[l] == 0) {
 				continue;
 			}
-			
+
 			// Starting at each base pair in the chunk
-			for (int j = 0; j < occ.length-i; j++) {
-				// Calculate the probability that this fragment is occupied by a nucleosome
-				// using the inclusion-exclusion principle with nucleosome width = 147bp;
-				float pOccupied = InclusionExclusion.independent(occ, j, j+i);
+			for (int x = maxL - extend; x < pNuc.length - maxL; x++) {
 				// Calculate the probability that this fragment survives FAIRE
-				float pFAIRE = 1 - crosslink*pOccupied;
-				// And weight the probability by the sonication distribution
-				float pFragment = sonication[i]*pFAIRE;
-				
-				// Add its probability at the +/- ends, weighted by fragment abundance
-				watson[j] += pFragment;
-				crick[j+i-1] += pFragment;
+				// and add its probability to the prediction,
+				// weighted by the sonication distribution
+				float pFragment = sonication[l] * pFAIRE(x, l);
+				watson[x] += pFragment;
+				crick[x + l - 1] += pFragment;
 			}
 		}
-		
+
+		log.debug("Extending watson and crick strands to produce single-end FAIRE signal");
 		float[] prediction = new float[chunk.length()];
-		for (int i = 0; i < occ.length; i++) {
+		for (int i = 0; i < pNuc.length; i++) {
 			for (int j = 0; j < extend; j++) {
 				// Extend on the + strand
-				if (i+j-maxL > 0 && i+j-maxL < prediction.length) {
-					prediction[i+j-maxL] += watson[i];
+				if (i + j - maxL > 0 && i + j - maxL < prediction.length) {
+					prediction[i + j - maxL] += watson[i];
 				}
-				
+
 				// Extend on the - strand
-				if (i-j-maxL > 0 && i-j-maxL < prediction.length) {
-					prediction[i-j-maxL] += crick[i];
+				if (i - j - maxL > 0 && i - j - maxL < prediction.length) {
+					prediction[i - j - maxL] += crick[i];
 				}
 			}
 		}
-		
+
 		return prediction;
 	}
-	
+
 	/**
 	 * Paired-end, extend to the actual length of each fragment
-	 * @return
+	 * 
+	 * @return paired-end FAIRE signal for the chunk
 	 */
-	private float[] paired(Interval chunk, float[] occ) {
+	private float[] paired(Interval chunk) {
 		float[] prediction = new float[chunk.length()];
 		// Consider all possible fragment lengths
-		for (int i = minL; i < sonication.length; i++) {
+		for (int l = minL; l < sonication.length; l++) {
 			// No need to count if there are no fragments of this length
-			if (sonication[i] == 0) {
+			if (sonication[l] == 0) {
 				continue;
 			}
-			
+
 			// Starting at each base pair in the chunk
-			for (int j = 0; j < occ.length-i; j++) {
-				// Calculate the probability that this fragment is occupied by a nucleosome
-				// using the inclusion-exclusion principle with nucleosome width = 147bp;
-				float pOccupied = InclusionExclusion.independent(occ, j, j+i);
+			for (int x = maxL - l; x < pNuc.length - maxL; x++) {
 				// Calculate the probability that this fragment survives FAIRE
-				float pFAIRE = 1 - crosslink*pOccupied;
-				// And weight the probability by the sonication distribution
-				float pFragment = sonication[i]*pFAIRE;
-				
-				// Add its probability to the occupancy
-				for (int k = 0; k < i; k++) {
-					if (j+k-maxL > 0 && j+k-maxL < prediction.length) {
-						prediction[j+k-maxL] += pFragment;
+				// and add its probability to the prediction,
+				// weighted by the sonication distribution
+				float pFragment = sonication[l] * pFAIRE(x, l);
+				for (int k = 0; k < l; k++) {
+					if (x - maxL + k > 0 && x - maxL + k < prediction.length) {
+						prediction[x - maxL + k] += pFragment;
 					}
 				}
 			}
 		}
-			
+
 		return prediction;
 	}
-	
-	
+
+	/**
+	 * The probability that a fragment of length l, starting at x, survives
+	 * phenol:chloroform, given the nucleosome distribution in pNuc
+	 */
+	private float pFAIRE(int x, int l) {
+		return 1 - crosslink * pOcc(x, l);
+	}
+
+	/**
+	 * The probability that a fragment of length l, starting at x, is occupied
+	 * by a nucleosome, given the nucleosome distribution in pNuc
+	 * 
+	 * pNuc[x] must be normalized so that the sum of any adjacent N base pairs
+	 * is <= 1 (i.e. nucleosomes cannot overlap)
+	 * 
+	 * pOcc[x] must be the convolution of pNuc with an N-base pair window, i.e.
+	 * the probability of being occupied for each base pair x
+	 */
+	private float pOcc(int x, int l) {
+		// Decompose the probability into the union of two events:
+		// 1) Is there a nucleosome in [x-N/2,x+N/2) or
+		// 2) Is the fragment starting at x+N with length l-N occupied
+		// P(1 or 2) = P(1) + P(2) - P(1 and 2)
+
+		// Base case for recursion (the end of a fragment)
+		if (l <= 0) {
+			float p = 0;
+			// log.debug("x = "+x+", l = "+l);
+			for (int i = x - nucSize / 2; i < x + l + nucSize / 2; i++) {
+				p += pNuc[i];
+			}
+			return p;
+		}
+
+		// Check for a cached result
+		if (pOccCache.isCached(x, l)) {
+			return pOccCache.getCache(x, l);
+		}
+
+		// 1. Is there a nucleosome in [x-N/2,x+N/2)?
+		// Since nucleosomes cannot overlap, these events are all independent
+		// so sum the probabilities, i.e. calculate the occupancy of x
+		float p1 = pOcc[x];
+
+		// 2. Is the fragment starting at x+N with length l-N occupied? Recurse
+		float p2 = pOcc(x + nucSize, l - nucSize);
+
+		// 1 and 2. What is the probability that there is a nucleosome in
+		// [x-N/2,x+N/2)
+		// AND the fragment starting at x+N with length l-N is occupied?
+		// Break it down by each mutually exclusive x+j case, for j \in
+		// [-N/2,N/2)
+		float p1Andp2 = 0;
+		if (pAndCache.isCached(x, l)) {
+			p1Andp2 = pAndCache.getCache(x, l);
+		} else {
+			for (int j = -nucSize / 2; j < nucSize / 2; j++) {
+				p1Andp2 += pNuc[x + j] * pOcc(x + j + nucSize, l - j - nucSize);
+			}
+			pAndCache.setCache(x, l, p1Andp2);
+		}
+
+		// Cache the result
+		float p = p1 + p2 - p1Andp2;
+		pOccCache.setCache(x, l, p);
+
+		return p;
+	}
+
 	/**
 	 * @param args
-	 * @throws WigFileException 
-	 * @throws IOException 
+	 * @throws WigFileException
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException, WigFileException {
 		new PredictFAIRESignal().instanceMain(args);
+	}
+
+	/**
+	 * A memoization cache for a function that takes values on a 2D lattice in
+	 * \mathbb{N}^2
+	 */
+	private static class Cache {
+
+		private float[][] cache;
+
+		public Cache(int sizeX, int sizeY) {
+			cache = new float[sizeX][sizeY];
+			resetCache();
+		}
+
+		public void resetCache() {
+			// Set all values to -1 to mark them as uncalculated
+			for (int i = 0; i < cache.length; i++) {
+				for (int j = 0; j < cache[i].length; j++) {
+					cache[i][j] = -1;
+				}
+			}
+		}
+
+		public boolean isCached(int x, int y) {
+			return cache[x][y] >= 0 || Float.isNaN(cache[x][y]);
+		}
+
+		public float getCache(int x, int y) {
+			return cache[x][y];
+		}
+
+		public void setCache(int x, int y, float v) {
+			cache[x][y] = v;
+		}
+
+		public float density() {
+			int nElements = cache.length * cache[0].length;
+			int nCached = 0;
+			for (int i = 0; i < cache.length; i++) {
+				for (int j = 0; j < cache[i].length; j++) {
+					if (isCached(i, j)) {
+						nCached++;
+					}
+				}
+			}
+
+			return ((float) nCached) / nElements;
+		}
 	}
 
 }
