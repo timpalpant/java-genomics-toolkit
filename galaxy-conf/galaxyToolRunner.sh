50f88f011ab450ef2b68f39f3f48ed40351c5c5d
Timothy Palpant <tim@palpant.us>
2019-02-09 13:50:03 -0500

    Remove Java 7 check

--- galaxy-conf/galaxyToolRunner.sh
+++ galaxy-conf/galaxyToolRunner.sh
@@ -6,13 +6,5 @@ then
   exit;
 fi
 
-# Verify that the user has Java 7 installed
-# Otherwise there will be an obscure UnsupportedClassVersion error
-version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
-if [[ "$version" < "1.7" ]]; then
-    echo "Need Java 7 or greater. You have Java $version installed."
-    exit
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 java -Dlog4j.configuration=log4j.properties -cp $DIR:$DIR/../build:$DIR/../dist/*:$DIR/../lib/* edu.unc.genomics."$@"
